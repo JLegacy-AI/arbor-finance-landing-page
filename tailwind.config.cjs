f2912ef588734544b155a655f672416925dae86e
jamal <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2022-09-06 23:15:04 +0500

    After Adding Slider and Upgrade

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,25 +4,38 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'primary-color':'#0D120B',
-        'primary-color-2':'#0F2923',
+        'primary-color':'#081613',
+        'primary-color-2':'#0F2822',
+        'primary-color-3':'#030D18',
         'secondary-color': '#54CC94',
         'tertiary-color': '#047B62',
         'tertiary-color-2': '#8DB9B0',
+
       },
+      // background: #030D18;
+
       backgroundImage:{
-        'hero-section': "url('./assets/images/Hero section image.png')",
+        'hero-section': "url('./assets/images/background-image.png')",
         'mission-section': "url('./assets/images/Mission.png')",
         'frame-1': "url('./assets/images/Frame-1.png')",
         'frame-2': "url('./assets/images/Frame-2.png')",
         'frame-3': "url('./assets/images/Frame-3.png')",
+        'gradient-radial': 'radial-gradient(ellipse at center, var(--tw-gradient-stops))', 
+        'gradient-radial-at-t': 'radial-gradient(ellipse at top, var(--tw-gradient-stops))', 
+        'gradient-radial-at-b': 'radial-gradient(ellipse at bottom, var(--tw-gradient-stops))', 
+        'gradient-radial-at-l': 'radial-gradient(ellipse at left, var(--tw-gradient-stops))', 
+        'gradient-radial-at-r': 'radial-gradient(ellipse at right, var(--tw-gradient-stops))', 
+        'gradient-radial-at-tl': 'radial-gradient(ellipse at top left, var(--tw-gradient-stops))', 
+        'gradient-radial-at-tr': 'radial-gradient(ellipse at top right, var(--tw-gradient-stops))', 
+        'gradient-radial-at-bl': 'radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))', 
+        'gradient-radial-at-br': 'radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))', 
       },
       animation: {
         'slide': 'slide 50s linear infinite',
       },
       keyframes: {
         slide: {
-          '0%': { transform: 'translateXY(0px 25px)'},
+          '0%': { transform: 'translateX(0px)'},
           '100%': { transform: 'translateX(-100%)'},
         }
       }
